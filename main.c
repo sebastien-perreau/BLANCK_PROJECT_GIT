0d6c82fc390a5169f0596507eb03c8464dc06a32
Sebastien <44413525+sebastien-perreau@users.noreply.github.com>
2020-09-14 14:18:27 +0200

    200914

--- main.c
+++ main.c
@@ -2,8 +2,7 @@
 #include "config.h"
 
 ACQUISITIONS_DEF(acquisitions);
-BLE_PICKIT_DEF(ble_params, "Home box");
-
+BLE_PICKIT_DEF(ble_pickit, "Home box", ENABLE);
 
 SOFTWARE_PWM_DEF(spwm, TIMER5, SOFTWARE_PWM_FREQ_200HZ, SOFTWARE_PWM_RESO_1, __PB12);
 LED_DEF(stairs_led, &spwm.pwm[0], OFF, 100, TICK_10MS, TICK_5MS);
@@ -12,6 +11,13 @@ AVERAGE_DEF(stairs_sensor_bottom, AN13, 30, TICK_1MS);
 SWITCH_DEF(sw_rolling_shutters_open, SWITCH1, ACTIVE_LOW);
 SWITCH_DEF(sw_rolling_shutters_close, SWITCH2, ACTIVE_LOW);
 static uint64_t tick_led = (TICK_1S * 60);
+
+static uint8_t rolling_shutters_previous_states = 0;
+static uint8_t rolling_shutter_kitchen_state = 0;
+static uint8_t rolling_shutter_dining_room_state = 0;
+static uint8_t rolling_shutter_living_room_state = 0;
+
+uint32_t secure_validation(uint8_t * public_key);
 
 int main(void)
 {        
@@ -22,16 +28,26 @@ int main(void)
     
     // Others initializations
     pwm_init(PWM_NONE, 40000, 250000);
-    ble_init(UART4, UART_BAUDRATE_1M, &ble_params);
+    ble_init(UART4, UART_BAUDRATE_1M, &ble_pickit);
     log_init(UART1, UART_BAUDRATE_2M);
     m_init_hardware_picadapter();
     mUpdateLedStatusD2(OFF);
     mUpdateLedStatusD3(BLINK);          
     
-    software_pwm_init(&spwm);    
+    software_pwm_init(&spwm);
+    
+    mInitIOAsOutput(__PB10);
+    mInitIOAsOutput(__PB11);
+    
+    mInitIOAsOutput(__PB8);
+    mInitIOAsOutput(__PB9);
+    
+    mInitIOAsOutput(__PB4);
+    mInitIOAsOutput(__PB5);
     
     while(1)
     {
+        static uint64_t tick_timeout = 0;
         
         if (((uint16_t) stairs_sensor_bottom.average < 100) && ((uint16_t) stairs_sensor_top.average < 100))
         {
@@ -55,12 +71,71 @@ int main(void)
         fu_adc_average(&stairs_sensor_top);
         fu_adc_average(&stairs_sensor_bottom);
         fu_led(&stairs_led);
-        fu_switch(&sw_rolling_shutters_open);
-        fu_switch(&sw_rolling_shutters_close);        
+        
+        mLatIO(__PB10) = GET_BIT(rolling_shutter_kitchen_state, 0);
+        mLatIO(__PB11) = GET_BIT(rolling_shutter_kitchen_state, 1);
+        
+        mLatIO(__PB8) = GET_BIT(rolling_shutter_dining_room_state, 0);
+        mLatIO(__PB9) = GET_BIT(rolling_shutter_dining_room_state, 1);
+        
+        mLatIO(__PB4) = GET_BIT(rolling_shutter_living_room_state, 0);
+        mLatIO(__PB5) = GET_BIT(rolling_shutter_living_room_state, 1);
+        
+        if (ble_pickit.app_buffer.in_is_updated)
+        {
+            ble_pickit.app_buffer.in_is_updated = false;
+            
+            if (ble_pickit.app_buffer.in_length == 3)
+            {
+                mUpdateTick(tick_timeout);
+                if ((ble_pickit.app_buffer.in_data[0] >= 0) && (ble_pickit.app_buffer.in_data[0] <= 2))
+                {
+                    rolling_shutter_kitchen_state = ble_pickit.app_buffer.in_data[0];
+                }
+                if ((ble_pickit.app_buffer.in_data[1] >= 0) && (ble_pickit.app_buffer.in_data[1] <= 2))
+                {
+                    rolling_shutter_dining_room_state = ble_pickit.app_buffer.in_data[1];
+                }
+                if ((ble_pickit.app_buffer.in_data[2] >= 0) && (ble_pickit.app_buffer.in_data[2] <= 2))
+                {
+                    rolling_shutter_living_room_state = ble_pickit.app_buffer.in_data[2];
+                }
+            }     
+        }  
+        else if (mTickCompare(tick_timeout) >= (TICK_1S * 30))
+        {
+            rolling_shutter_kitchen_state = 0;
+            rolling_shutter_dining_room_state = 0;
+            rolling_shutter_living_room_state = 0;
+        }
+                
+        if (rolling_shutters_previous_states != ((rolling_shutter_kitchen_state << 0) | (rolling_shutter_dining_room_state << 2) | (rolling_shutter_living_room_state << 4)))
+        {
+            rolling_shutters_previous_states = (rolling_shutter_kitchen_state << 0) | (rolling_shutter_dining_room_state << 2) | (rolling_shutter_living_room_state << 4);
+            ble_pickit.app_buffer.out_length = 3;
+            ble_pickit.app_buffer.out_data[0] = rolling_shutter_kitchen_state;
+            ble_pickit.app_buffer.out_data[1] = rolling_shutter_dining_room_state;
+            ble_pickit.app_buffer.out_data[2] = rolling_shutter_living_room_state;
+            ble_pickit.flags.notif_app_buffer = 1;
+        }
         
         ble_stack_tasks();
                
         background_tasks(&acquisitions);
         
     }
+}
+
+uint32_t secure_validation(uint8_t * public_key)
+{
+    static const uint16_t private_key_128bits[16] = {6627, 6855, 3424, 7121, 6054, 9187, 2013, 4725, 625, 132, 9827, 5467, 3827, 8992, 65, 1087};
+    uint32_t result = 0;
+    uint8_t i;
+    
+    for (i = 0 ; i < 16 ; i++)
+    {
+        result += (public_key[i] * private_key_128bits[i]);
+    }
+    
+    return result;
 }
